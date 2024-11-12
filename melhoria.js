ae3ee86914dd8222f50e1aa7aeedfa6d8c40e346
Maria Eduarda Lopes Constantino <160516429+Marialopesz@users.noreply.github.com>
2024-11-12 09:16:08 -0300

    Add files via upload

--- melhoria.js
+++ melhoria.js
@@ -1,40 +1,61 @@
-
-// Função original 
+// Função original
 function gerarAleatoriosOriginal(qtd = 6) {
-    var vetor = [];
-    while (vetor.length < qtd) {
-        var aleatorio = Math.floor(Math.random() * 60 + 1);
-        if (!vetor.includes(aleatorio)) {
-            vetor.push(aleatorio);
-        }
+  var vetor = [];
+  while (vetor.length < qtd) {
+    var aleatorio = Math.floor(Math.random() * 60 + 1);
+    if (vetor.includes(aleatorio)) {
+      continue;
     }
-    return vetor;
+    {
+      vetor.push(aleatorio);
+    }
+  }
+  return vetor;
 }
 
-// Função otimizada 
+// Função otimizada
 function gerarAleatoriosOtimizadoComSet(qtd = 6, max = 60) {
-    const numerosAleatorios = new Set();
-    while (numerosAleatorios.size < qtd) {
-        numerosAleatorios.add(Math.floor(Math.random() * max) + 1);
-    }
-    return Array.from(numerosAleatorios);
+  const numerosAleatorios = new Set();
+  while (numerosAleatorios.size < qtd) {
+    numerosAleatorios.add(Math.floor(Math.random() * max) + 1);
+  }
+  return Array.from(numerosAleatorios);
 }
 
-// Função para medir o tempo de execução 
-function tempoExecucao(funcao) {
+// Função para medir o tempo médio de várias execuções
+function tempoExecucaoMedio(funcao, qtdExecucoes = 100, qtdNumeros = 50) {
+  let totalTempo = 0;
+
+  for (let i = 0; i < qtdExecucoes; i++) {
     const inicio = performance.now();
-    funcao();
+    funcao(qtdNumeros);
     const fim = performance.now();
-    return fim - inicio;
+    totalTempo += fim - inicio;
+  }
+
+  return totalTempo / qtdExecucoes;
 }
 
+// Executar a função várias vezes para obter a média
+const execucoes = 1000;
+const tempoOriginalMedio = tempoExecucaoMedio(
+  gerarAleatoriosOriginal,
+  execucoes
+);
+const tempoOtimizadoComSetMedio = tempoExecucaoMedio(
+  gerarAleatoriosOtimizadoComSet,
+  execucoes
+);
 
-const tempoOriginal = tempoExecucao(() => gerarAleatoriosOriginal(50));
-const tempoOtimizadoComSet = tempoExecucao(() => gerarAleatoriosOtimizadoComSet(50));
+const melhoriaMedio =
+  ((tempoOriginalMedio - tempoOtimizadoComSetMedio) / tempoOriginalMedio) * 100;
 
-const melhoria = ((tempoOriginal - tempoOtimizadoComSet) / tempoOriginal) * 100;
-
-
-console.log(`Tempo Original: ${tempoOriginal.toFixed(2)} ms`);
-console.log(`Tempo Otimizado com Set: ${tempoOtimizadoComSet.toFixed(2)} ms`);
-console.log(`Melhoria com Set: ${melhoria.toFixed(2)}%`);
+console.log(
+  `Tempo Médio - Função Original: ${tempoOriginalMedio.toFixed(5)} ms`
+);
+console.log(
+  `Tempo Médio - Função Otimizada com Set: ${tempoOtimizadoComSetMedio.toFixed(
+    5
+  )} ms`
+);
+console.log(`Melhoria Percentual com Set: ${melhoriaMedio.toFixed(2)}%`);
